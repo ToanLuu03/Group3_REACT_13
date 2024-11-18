09b6d77c748f1d2b089daca9f05727580ee18fce
Luu Truong Toan <luutruongtoanltt@gmail.com>
2024-11-18 10:18:11 +0700

    new 18/11

--- src/services/instance.js
+++ src/services/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "http://fams-app.ap-southeast-2.elasticbeanstalk.com/api",
+  baseURL: "https://fams-app.ap-southeast-2.elasticbeanstalk.com/api",
 });
 
 instance.interceptors.response.use(function (response) {
